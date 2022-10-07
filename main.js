0040fcc87bc4b8570a795fe4df2e12d80801f1bc
Jesier <jesiersantiago@gmail.com>
2022-10-07 09:43:10 -0500

    push up

---
+++ main.js
@@ -0,0 +1,12 @@
+const dishes = [
+    "Dinner plate", "Water glass", "Salad bowl", 
+    "Dinner plate", "Wine glass", "Spoon", 
+    "Spoon", "Fork", "Salad bowl", "Whiskey glass", 
+    "Fork", "Spoon", "Fork"
+]
+
+const dirtyStuff = [dishes[1], dishes[4], dishes[9]]
+
+console.log(dirtyStuff[0])
+console.log(dirtyStuff[1])
+console.log(dirtyStuff[2])
